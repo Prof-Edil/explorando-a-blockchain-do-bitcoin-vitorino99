2819934520731284c2732bfe8190c80e24045c4b
vitorino99 <vitorcamilolemos@gmail.com>
2025-02-01 16:45:33 -0300

    Solução 6 e alteração na 5

--- submission/005.sh
+++ submission/005.sh
@@ -1,4 +1,16 @@
 # Create a 1-of-4 P2SH multisig address from the public keys in the four inputs of this tx:
 #   `37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517`
-bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_270 -rpcpassword=vW8nQdLUWBaJ getrawtransaction 37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517 true
-echo "$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_270 -rpcpassword=vW8nQdLUWBaJ createmultisig 1 '["02bbb4ba3f39b5f3258f0014d5e4eab5a6990009e3e1dba6e8eaff10b3832394f7", "03aaf17b1a7b4108f7e5bc4f7d59c20f7fb1a72dbc74a9a3d6d1f8488df159c760", "03a6d919c76d9117c23570a767450013edf31cf6be7d3b5a881c06a9aa1f2c24ce", "0383d12258e3e294a6d7754336f6b4baef992ec4b91694d3460bcb022b11da8cd2"]')"
+
+txid="37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517"
+
+# Obter os detalhes da transação em JSON
+transaction=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_270 -rpcpassword=vW8nQdLUWBaJ getrawtransaction $txid true)
+
+# Extrair as chaves públicas dos inputs usando jq
+keys=$(echo $transaction | jq -r '[.vin[].txinwitness[1]] | unique')
+
+# Criar o endereço multisig 1-de-4
+multisig=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_270 -rpcpassword=vW8nQdLUWBaJ createmultisig 1 "$keys")
+
+# Exibir o endereço P2SH
+echo $multisig | jq -r .address
--- submission/006.sh
+++ submission/006.sh
@@ -1,1 +1,14 @@
-# Which tx in block 257,343 spends the coinbase output of block 256,128?
+# Obter o hash do bloco 256128
+hash1=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_270 -rpcpassword=vW8nQdLUWBaJ getblockhash 256128)
+
+# Obter a transação coinbase do bloco 256128 (tx[0])
+txid=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_270 -rpcpassword=vW8nQdLUWBaJ getblock $hash1 2 | jq -r '.tx[0].txid')
+
+# Obter o hash do bloco 257343
+hash2=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_270 -rpcpassword=vW8nQdLUWBaJ getblockhash 257343)
+
+# Buscar a transação que gasta a saída da coinbase do bloco 256128
+resp=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_270 -rpcpassword=vW8nQdLUWBaJ getblock $hash2 2 | jq -r --arg TXID "$txid" '.tx[] | select(.vin[].txid == $TXID) | .txid')
+
+# Exibir o TXID da transação que gasta a coinbase
+echo $resp

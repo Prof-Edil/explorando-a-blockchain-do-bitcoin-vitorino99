58a62bb3dbb6b4d8992aebc0bf1bb5cb63c60dee
vitorino99 <vitorcamilolemos@gmail.com>
2025-02-02 15:15:29 -0300

    Solução 8

--- submission/008.sh
+++ submission/008.sh
@@ -1,8 +1,4 @@
-transaction_raw=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_270 -rpcpassword=vW8nQdLUWBaJ getrawtransaction 37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517)
-public_keys=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_270 -rpcpassword=vW8nQdLUWBaJ decoderawtransaction $transaction_raw | jq -c '[.vin[].txinwitness[1]]')
-
-resp=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_270 -rpcpassword=vW8nQdLUWBaJ createmultisig 1 $public_keys | jq -r '.address')
-
+# Which public key signed input 0 in this tx:
+#   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
+resp=$( bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_270 -rpcpassword=vW8nQdLUWBaJ getrawtransaction e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163 1 | jq -r '.vin[0].txinwitness[2]' | cut -c 5-70)
 echo $resp
-
-# '["02bbb4ba3f39b5f3258f0014d5e4eab5a6990009e3e1dba6e8eaff10b3832394f7", "03aaf17b1a7b4108f7e5bc4f7d59c20f7fb1a72dbc74a9a3d6d1f8488df159c760","03a6d919c76d9117c23570a767450013edf31cf6be7d3b5a881c06a9aa1f2c24ce","0383d12258e3e294a6d7754336f6b4baef992ec4b91694d3460bcb022b11da8cd2"]'

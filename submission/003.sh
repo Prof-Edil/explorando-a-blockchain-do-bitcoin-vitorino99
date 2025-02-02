a44b25f5b01b9cf0c50ce29f19a50e1ddf6b04e3
vitorino99 <vitorcamilolemos@gmail.com>
2025-02-02 15:23:48 -0300

    Alterações nas soluções

--- submission/003.sh
+++ submission/003.sh
@@ -1,3 +1,7 @@
 # How many new outputs were created by block 123,456?
-echo "$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_270 -rpcpassword=vW8nQdLUWBaJ getblock 0000000000002917ed80650c6174aac8dfc46f5fe36480aaef682ff6cd83c3ca 2 | jq '[.tx[] | .vout | length] | add')"
+hash=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_270 -rpcpassword=vW8nQdLUWBaJ getblockhash 123456)
+resp=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_270 -rpcpassword=vW8nQdLUWBaJ getblock $hash 2 | jq '[.tx[] | .vout | length] | add')
+
+echo $resp
+
 
--- submission/007.sh
+++ submission/007.sh
@@ -1,16 +1,11 @@
-# Obter o hash do bloco 123321
+# Only one single output remains unspent from block 123,321. What address was it sent to?
 block_hash=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_270 -rpcpassword=vW8nQdLUWBaJ getblockhash 123321)
-
-# Obter todas as transações do bloco 123321
 block_data=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_270 -rpcpassword=vW8nQdLUWBaJ getblock $block_hash 2)
 
-# Percorrer todas as transações do bloco e verificar qual saída ainda não foi gasta
 unspent_output=""
 echo "$block_data" | jq -c '.tx[] | {txid: .txid, vout: .vout[]}' | while read utxo; do
     txid=$(echo "$utxo" | jq -r '.txid')
     vout_index=$(echo "$utxo" | jq -r '.vout.n')
-
-    # Consultar se a saída ainda está na UTXO set
     utxo_check=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_270 -rpcpassword=vW8nQdLUWBaJ gettxout "$txid" "$vout_index")
 
     if [[ -n "$utxo_check" ]]; then
